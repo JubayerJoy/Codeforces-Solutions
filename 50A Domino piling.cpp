44cd82bf721fffcef96cef5a799509eb4606a75d
Jubayer Abdullah Joy <jubayerjoy98@gmail.com>
2021-10-01 05:36:06 +0600

    Add 50A
    * Add 50A
    
    * Fix solution 8 link

---
+++ 50A Domino piling.cpp
@@ -0,0 +1,20 @@
+/**
+  * Author     : Jubayer Abdullah Joy
+  *              CSE, 5th Batch, University of Barishal
+  * Mail       : jubayerjoy98@gmail.com
+  * Codeforces : Joy
+  * Github     : github.com/JubayerJoy
+  * Problem    : 
+  *
+  */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    scanf("%d %d",&a,&b);
+    printf("%d\n",a*b/2);
+    return 0;
+}
